53491e723923a0df424c56c101250d366ff720bc
Leo Doan <doanleo@yahoo.com>
2022-04-11 18:06:28 -0400

    Refactor the DL and update UI. Also made Cart

---
+++ StoreApp/Models/Cart.cs
@@ -0,0 +1,13 @@
+namespace Models
+{
+    public class Cart
+    {
+        // A Dictionary?
+        // product and number. 
+
+        //add to cart method(prod, value)
+        //remove from cart method(prod, value)
+        //display cart
+
+    }
+}
